f13d5b9fa1f85c4b3da0f0d9fc1819d56df496d8
Kalpeshk22 <kalpeshkshirsagar7824@gmail.com>
2026-07-21 15:58:51 +0530

    first commit

---
+++ LongestValidParentheses.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+
+public class LongestValidParentheses {
+
+    public static int longestValidParentheses(String s) {
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+
+        int ans = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+
+                stack.pop();
+
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    ans = Math.max(ans, i - stack.peek());
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(longestValidParentheses("(()"));
+        System.out.println(longestValidParentheses(")()())"));
+        System.out.println(longestValidParentheses(""));
+    }
+}
